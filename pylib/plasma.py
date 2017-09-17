546fe78f336c8e9dfd4392b2cb43bab7b411c6af
Peter Williams <peter@newton.cx>
2017-09-17 13:19:14 -0400

    pylib/plasma.py: more work here

--- pylib/plasma.py
+++ pylib/plasma.py
@@ -12,9 +12,11 @@ from __future__ import absolute_import, division, print_function
 __all__ = '''
 Modes
 Parameters
+cutoff_frequencies
 omega_plasma
 omega_cyclotron
 phase_velocity
+resonance_frequencies
 wavelength
 wavenumber
 '''.split()
@@ -49,6 +51,10 @@ def omega_cyclotron(q, B, mass):
       The mass of each particle, in g.
     Returns:
       The cyclotron frequency, in rad/s.
+
+    We follow the convention of Stix and some other authors in that this value
+    has a sign that depends on the sign of the charge of the species in
+    question.
 
     """
     return q * cgs.e * B / (mass * cgs.c)
@@ -85,6 +91,131 @@ def wavenumber(refractive_index, omega):
 
     """
     return refractive_index * omega / cgs.c
+
+
+def cutoff_frequencies(n_e, B, gamma=1.):
+    """Calculate the wave cutoff frequencies associated with a plasma.
+
+    n_e
+      The number density of electrons in cm^-3. Neutrality is
+      assumed so this is also the number density of protons.
+    B
+      The ambient magnetic field strength in Gauss.
+    gamma
+      The Lorentz factor of the electrons. This is used to
+      adjust effective mass of the electrons. The protons are
+      assumed to have an effective Lorentz factor of unity.
+    Returns:
+      A sorted, 1D array of up to three cutoff frequencies in rad/s. At low
+      densities the highest-frequency cutoff is pretty much the electron
+      cyclotron frequency. Divide by (2 * pi * 1e9) to get GHz.
+
+    Waves that propagate into a cutoff are reflected
+    (https://farside.ph.utexas.edu/teaching/plasma/lectures1/node48.html).
+
+    Based on setting R, L, and P = 0 in Stix equations (2-1)--(2-3). The first
+    two can be converted into quadratics in omega, and they differ only by the
+    sign of B.
+
+    """
+    m_e = gamma * cgs.me
+    m_i = cgs.mp
+    n_i = n_e
+
+    om_pe = omega_plasma(n_e, m_e)
+    om_pi = omega_plasma(n_i, m_i)
+    om_ce = omega_cyclotron(-1, B, m_e)
+    om_ci = omega_cyclotron(+1, B, m_i)
+
+    cutoffs = [np.sqrt(om_pe**2 + om_pi**2)] # P = 0 cutoff is trivial.
+
+    A = 1.
+    B = -(om_ce + om_ci) # this is the L = 0 cutoff; we're destroying the magnetic field variable
+    C = om_ce * om_ci - om_pe**2 - om_pi**2
+
+    if 4 * A * C  > B**2:
+        return np.array(cutoffs) # no other valid solutions.
+
+    # Between R and L and the +/- in the quadratic equations, there are four
+    # possible solutions, two of which are negations of the other, so there
+    # are always two nonnegative solutions. If RHS == 0 they're the same
+    # number, though. `B` and `RHS` as we've defined them are always
+    # nonnegative.
+
+    prefactor = 1. / (2 * A)
+    rhs = np.sqrt(B**2 - 4 * A * C)
+
+    cutoffs.append(prefactor * (B + rhs))
+
+    if rhs != 0.:
+        if rhs > B:
+            cutoffs.append(prefactor * (rhs - B))
+        else:
+            cutoffs.append(prefactor * (B - rhs))
+
+    return np.array(sorted(cutoffs))
+
+
+def resonance_frequencies(n_e, B, theta, gamma=1.):
+    """Calculate the wave resonance frequencies associated with propagation in a
+    particular plasma.
+
+    n_e
+      The number density of electrons in cm^-3. Neutrality is
+      assumed so this is also the number density of protons.
+    B
+      The ambient magnetic field strength in Gauss.
+    theta
+      The angle of wave propagation relative to the magnetic field, in
+      radians. The resonance condition depends on this value.
+    gamma
+      The Lorentz factor of the electrons. This is used to
+      adjust effective mass of the electrons. The protons are
+      assumed to have an effective Lorentz factor of unity.
+    Returns:
+      A sorted, 1D array of up to three resonance frequencies in rad/s.
+      (Probably the actual number of such resonances is always 1 or 0.) Divide
+      by (2 * pi * 1e9) to get GHz.
+
+    This usually works out to be around the electron plasma frequency as theta
+    => 0., and a smaller value as theta => 90.
+
+    FIXME: more intuitive understanding of how these numbers work out in the
+    various parameter limits (e.g. low densities, theta => 0, theta => 90
+    degrees, etc.).
+
+    Waves that propagate into a resonance are absorbed, heating the plasma
+    (https://farside.ph.utexas.edu/teaching/plasma/lectures1/node48.html).
+
+    Based on the solution of Stix equation (1-45) with plasma parameters
+    determined from equations (1-19) and (2-1)--(2-3). Some naive algebra
+    converts the condition into a cubic in the square of omega.
+
+    """
+    m_e = gamma * cgs.me
+    m_i = cgs.mp
+    n_i = n_e
+
+    om_pe = omega_plasma(n_e, m_e)
+    om_pi = omega_plasma(n_i, m_i)
+    om_ce = omega_cyclotron(-1, B, m_e)
+    om_ci = omega_cyclotron(+1, B, m_i)
+
+    q = np.tan(theta)**2
+    j2 = om_ce**2 + om_ci**2
+    k2 = om_pe**2 + om_pi**2
+
+    c3 = q + 1
+    c2 = -(q + 1) * (j2 + k2)
+    c1 = (q + 1) * om_ce**2 * om_ce**2 - q * k2 * om_ce * om_ci + k2 * j2
+    c0 = -k2 * om_ce**2 * om_ce**2
+
+    roots = np.roots([c3, c2, c1, c0])
+    z = roots[np.abs(roots.imag) / np.abs(roots) < 1e-8].real
+    z = z[z > 0]
+    z = np.sort(z)
+    return np.sqrt(z)
+
 
 class _Modes(object):
     FAST = 0
@@ -124,7 +255,7 @@ class Parameters(object):
         gamma
           The Lorentz factor of the electrons. This is used to
           adjust effective mass of the electrons. The protons are
-          assumed to have an effective Lorentz factor of zero.
+          assumed to have an effective Lorentz factor of unity.
         Returns:
           A new Parameters instance.
 
@@ -143,8 +274,8 @@ class Parameters(object):
 
         alpha = om_pe**2 / omega**2
         beta = om_ci / omega
-        #gamma = (om_pi / om_ci)**2
-        mu = np.abs(om_ce / om_ci)
+        #gamma = (om_pi / om_ci)**2 -- defined by Stix but not needed
+        mu = np.abs(om_ce / om_ci) # = m_p / m_e ~ 43^2
 
         obj = cls()
         obj.omega = omega
