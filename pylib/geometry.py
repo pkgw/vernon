ade7d3e6a3da7cc26409296563a30fde58432b21
Peter Williams <peter@newton.cx>
2017-09-21 17:00:05 -0400

    pylib/geometry.py: right; need to clip fitted parameters
    This is a hack and I should devise a better solution.

--- pylib/geometry.py
+++ pylib/geometry.py
@@ -1512,6 +1512,7 @@ class PrecomputedImageMaker(ImageMaker):
         self.ds = h5py.File(h5path)
         self.cur_frame_group = self.ds['/frame0000']
         self.ny, self.nx = self.cur_frame_group['counts'].shape
+        print('CLIPPING DATA')
 
 
     def select_frame(self, new_frame_num):
@@ -1533,6 +1534,15 @@ class PrecomputedImageMaker(ImageMaker):
             if itemname == 'counts':
                 continue
 
-            setattr(ray, itemname, self.cur_frame_group[itemname][iy,ix,sl])
+            data = self.cur_frame_group[itemname][iy,ix,sl]
+
+            if itemname == 'p':
+                data = np.clip(data, 1.5, 7)
+            elif itemname == 'k':
+                data = np.clip(data, 0., 9)
+            elif itemname == 's':
+                data = np.clip(data, 1., 5e7)
+
+            setattr(ray, itemname, data)
 
         return ray
