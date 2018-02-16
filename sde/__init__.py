fc8dbcaa46a484b78310a98d96df78c52632431f
Peter Williams <peter@newton.cx>
2018-02-16 11:52:55 -0500

    sde: rationalize module structure

---
+++ sde/__init__.py
@@ -0,0 +1,27 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2018 Peter Williams and collaborators.
+# Licensed under the MIT License.
+
+"""Solving radiation belt populations using the stochastic differential
+equation approach.
+
+"""
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+
+
+# Command-line interface
+
+from pwkit.cli import die
+
+
+def entrypoint(argv):
+    if len(argv) == 1:
+        die('must supply a subcommand: "gen-grid"')
+
+    if argv[1] == 'gen-grid':
+        from .grid import gen_grid_cli
+        gen_grid_cli(argv[2:])
+    else:
+        die('unrecognized subcommand %r', argv[1])
