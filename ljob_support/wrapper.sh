a57b0e6f55b0a23dc0f230f230d11d203ca16b5e
Peter Williams <peter@newton.cx>
2018-04-11 23:36:22 -0400

    Add support for an "external" software stack
    This is to speed up my ljobs, because pulling all the Python file over NFS can
    be very painful. I *think* it's helping ...
    
    The ljob run that I did to test this change, though, gave huge numbers of
    "task requested next job before it finished the one it had" warnings. I can't
    understand why.

--- ljob_support/wrapper.sh
+++ ljob_support/wrapper.sh
@@ -41,6 +41,61 @@ ln -s $archive archive
 ln -s $jobinfo jobinfo
 ln -s $bulk bulk
 
+# If available, unpack a local version of the software stack since I/O to the
+# support files is an efficiency killer. Note that the binaries hardcode the
+# install prefix, so we can only unpack it in one place. This in turn means
+# that multiple workers running on the same node will share the same stack,
+# and it won't be tractable to be fancy about auto-detecting outdated software
+# or anything. Conversely, we do have to be fancy about different workers
+# stepping on each others' toes as they get set up.
+
+if [ -e $TOP/ljob_support/stack.tar.gz ] ; then
+    mkdir -p /scratch/pwilliam/vernon
+
+    # XXXX Temporary recovery from my busted initial efforts
+    if mkdir /scratch/pwilliam/vernon/fix_busted_stack 2>/dev/null ; then
+        echo "XXX won the right to remove old and busted stack"
+        if [ ! -f /scratch/pwilliam/vernon/fix_busted_stack/finished ] ; then
+            echo "XXX and fixing has not yet occurred"
+            rm -rf /scratch/pwilliam/vernon/stack
+            touch /scratch/pwilliam/vernon/fix_busted_stack/finished
+        fi
+    else
+        for ii in {0..100} ; do
+            if [ -f /scratch/pwilliam/vernon/fix_busted_stack/finished ] ; then
+                break
+            fi
+            sleep 2
+        done
+
+        if [ ! -f /scratch/pwilliam/vernon/fix_busted_stack/finished ] ; then
+            echo "XXX whooooa recovery broke?"
+            exit 1
+        fi
+    fi
+
+    if mkdir /scratch/pwilliam/vernon/stack 2>/dev/null ; then
+        # We won the race to unpack the stack on this node.
+        echo "Unpacking local stack $TOP/ljob_support/stack.tar.gz on $(hostname -s) ..."
+        tar xf $TOP/ljob_support/stack.tar.gz -C /scratch/pwilliam/vernon
+        touch /scratch/pwilliam/vernon/stack/unpacked.marker
+    else
+        for ii in {0..100} ; do
+            if [ -f /scratch/pwilliam/vernon/stack/unpacked.marker ] ; then
+                break
+            fi
+            sleep 2
+        done
+
+        if [ ! -f /scratch/pwilliam/vernon/stack/unpacked.marker ] ; then
+            echo "Stack never got unpacked?? ii=$ii"
+            exit 1
+        fi
+    fi
+
+    export VERNON_EXTERNAL_STACK=/scratch/pwilliam/vernon/stack
+fi
+
 # Go! We need to propagate SIGTERM to the Python process if we run out of time;
 # see http://veithen.github.io/2014/11/16/sigterm-propagation.html for explanation.
 
